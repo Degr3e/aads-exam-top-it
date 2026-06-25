94a2eed7e40da94cb2e21e3606b087846b2d32ed
Yugo <95368001+Degr3e@users.noreply.github.com>
2026-06-25 11:38:16 +0300

    add array create and free functions

--- yalovsky.gleb/U1/Array.hpp
+++ yalovsky.gleb/U1/Array.hpp
@@ -12,6 +12,22 @@ namespace yalovsky
     size_t size;
     size_t capacity;
   };
+
+  template< class T >
+  Array< T > makeArray()
+  {
+    Array< T > result{nullptr, 0, 0};
+    return result;
+  }
+
+  template< class T >
+  void freeArray(Array< T >& arr)
+  {
+    delete[] arr.data;
+    arr.data = nullptr;
+    arr.size = 0;
+    arr.capacity = 0;
+  }
 }
 
 #endif
